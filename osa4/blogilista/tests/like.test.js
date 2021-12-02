92a59519a3551d394acda3da1e296f08ee854649
Juho Alenius <juho.alenius@student.lab.fi>
2021-12-03 01:38:16 +0200

    4.5

--- osa4/blogilista/tests/like.test.js
+++ osa4/blogilista/tests/like.test.js
@@ -68,16 +68,6 @@ describe('total likes', () => {
 })
 
 describe('blog with most likes', () => {
-    const oneBlog = [
-        {
-            _id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 7,
-            __v: 0
-          }
-    ]
 
     test('of empty list is undefined', () => {
         const result = listHelper.favouriteBlog([])
